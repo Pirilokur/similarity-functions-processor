659a00d7c1d996b2fb33565b60723f790db4bd1e
Pirilokur <57696575+Pirilokur@users.noreply.github.com>
2020-05-01 01:13:58 -0400

    okapisimilarity updated

--- src/okapisimilarity.hpp
+++ src/okapisimilarity.hpp
@@ -6,10 +6,11 @@
 class OkapiSimilarity{
 	public:
 		OkapiSimilarity();
-
+		double okapi_calculate(std::string term);
 
 	private:
 		double idf_weight(std::string term);
 		double term_weight(std::string term);
 		double query_weight(std::string term);
+
 }
